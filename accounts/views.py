a2b211ffa1cbf7ffe20a908807825d783ba649d9
Caine Benoy <cainebenoy@gmail.com>
2025-01-20 21:31:15 +0530

    added files

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import SignUpForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'You have signed up successfully.')
+            return redirect('index')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
